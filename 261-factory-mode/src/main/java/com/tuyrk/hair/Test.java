118d374a1e0a651076ce2914f4d087859e3c6afd
tuyrk <tuyrk@qq.com>
2019-12-15 18:45:50 +0800

    261-模式的秘密---工厂模式(工厂模式)

--- 261-factory-mode/src/main/java/com/tuyrk/hair/Test.java
+++ 261-factory-mode/src/main/java/com/tuyrk/hair/Test.java
@@ -9,25 +9,25 @@ public class Test {
     public static void main(String[] args) {
         // 每需要创建一个发型都需要重新new，并且调用draw()
         /*HairInterface left = new LeftHair();
-        left.draw();
+        left.draw();// 左偏分发型
         HairInterface right = new RightHair();
-        right.draw();*/
+        right.draw();// 右偏分发型*/
 
         // if-else实现
         /*HairFactory factory = new HairFactory();
         HairInterface left = factory.getHair("left");
-        left.draw();*/
+        left.draw();// 左偏分发型*/
 
         // className实现
         /*HairFactory factory = new HairFactory();
          *//*HairInterface left = factory.getHairByClass("com.tuyrk.hair.impl.LeftHair");*//*
         HairInterface left = factory.getHairByClass(LeftHair.class.getName());
-        left.draw();*/
+        left.draw();// 左偏分发型*/
 
         // 读取properties实现
         HairFactory factory = new HairFactory();
-        HairInterface left = factory.getHairByClassKey("in");
-        left.draw();
+        HairInterface hair = factory.getHairByClassKey("in");
+        hair.draw();// 中分发型
 
     }
 }
